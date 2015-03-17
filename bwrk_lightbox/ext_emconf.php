5f46c179c0760b9ed55115f76ab843173945d697
Daniel Maier <dm@bergwerk.ag>
2015-03-17 17:08:07 +0100

    [TASK] Initial Commit

---
+++ bwrk_lightbox/ext_emconf.php
@@ -0,0 +1,34 @@
+<?php
+	
+	$EM_CONF[$_EXTKEY] = array(
+		'title' => 'Lightbox',
+		'description' => '',
+		'category' => 'fe',
+		'author' => 'BERGWERK [GD]',
+		'author_email' => 'gd@bergwerk.ag',
+		'author_company' => 'BERGWERK',
+		'shy' => 0,
+		'priority' => 'top',
+		'module' => '',
+		'state' => 'stable',
+		'internal' => 0,
+		'uploadfolder' => '0',
+		'createDirs' => '',
+		'modify_tables' => '',
+		'clearCacheOnLoad' => 0,
+		'lockType' => '',
+		'version' => '1.0.0',
+		'constraints' => array(
+			'depends' => array(
+				'extbase' => '6.0',
+				'fluid' => '6.0',
+				'typo3' => '6.0',
+			),
+			'conflicts' => array(
+			),
+			'suggests' => array(
+			),
+		),
+	);
+	
+?>
---
+++ bwrk_lightbox/ext_localconf.php
@@ -0,0 +1,16 @@
+<?php
+	if (!defined ('TYPO3_MODE')) die ('Access denied.');
+
+//	$TYPO3_CONF_VARS['FE']['eID_include']['tx_bwrk_produkte_view'] = 'EXT:bwrk_produkte/Classes/Controller/InsertControllerAjaxList.php';
+/*	
+	Tx_Extbase_Utility_Extension::configurePlugin(
+	    $_EXTKEY,
+	    'bwrk_lightbox_view',
+	    array(
+	        'Insert' => 'show',
+	    ),array(
+	        'Insert' => 'show',
+	    )
+	);
+*/
+?>
