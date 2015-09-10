35789ea295fe243f9aa2894f9a8800e820b9ad4a
Georg Dümmler <gd@bergwerk.ag>
2015-09-10 13:46:03 +0200

    Add Readme, Version Number, Clean the Extension and Copyright

---
+++ README.md
@@ -0,0 +1,12 @@
+# Lightbox 2
+
+## Installation
+1. Install the extension
+2. Include the extension-template in your TYPO3
+
+## How to use
+You only need to install the extension like the Installation-Guide.
+The Lightbox is activated automatically if you active the checkbox "enlarge on click" at any Image-Content-Element
+
+## Copyright of the Lightbox
+Lokesh Dhakar - http://lokeshdhakar.com/projects/lightbox2/
--- ext_tables.php
+++ ext_tables.php
@@ -1,10 +1,4 @@
 <?php
-	
-	if(!defined('TYPO3_MODE')) die ('Access denied.');
-	
-//	Tx_Extbase_Utility_Extension::registerPlugin($_EXTKEY, 'bwrk_produkte_view', 'Produkte Anzeige');
-//	Tx_Extbase_Utility_Extension::registerPlugin($_EXTKEY, 'bwrk_produkte_view_single', 'Produkte Anzeige (Single)');
-	
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'BERGWERK Lightbox');
-	
-?>
+
+if (!defined('TYPO3_MODE')) die ('Access denied.');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'BERGWERK Lightbox');
